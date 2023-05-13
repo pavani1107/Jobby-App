20abbfb7ba05d543a7408874ef60d25acddb66f0
pavani1107 <kallepavani@gmail.com>
2023-05-13 12:03:08 +0000

    jobby app

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import {Switch, Route, Redirect} from 'react-router-dom'
+import './App.css'
+import Login from './components/Login'
+import Home from './components/Home'
+import Jobs from './components/Jobs'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import JobItemDetails from './components/JobItemDetails'
+
+const employmentTypesList = [
+  {
+    label: 'Full Time',
+    employmentTypeId: 'FULLTIME',
+  },
+  {
+    label: 'Part Time',
+    employmentTypeId: 'PARTTIME',
+  },
+  {
+    label: 'Freelance',
+    employmentTypeId: 'FREELANCE',
+  },
+  {
+    label: 'Internship',
+    employmentTypeId: 'INTERNSHIP',
+  },
+]
+
+const salaryRangesList = [
+  {
+    salaryRangeId: '1000000',
+    label: '10 LPA and above',
+  },
+  {
+    salaryRangeId: '2000000',
+    label: '20 LPA and above',
+  },
+  {
+    salaryRangeId: '3000000',
+    label: '30 LPA and above',
+  },
+  {
+    salaryRangeId: '4000000',
+    label: '40 LPA and above',
+  },
+]
+
+const App = () => (
+  <div>
+    <Switch>
+      <ProtectedRoute exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/jobs" component={Jobs} />
+      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+      <Route exact path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
+    </Switch>
+  </div>
+)
+
+export default App
